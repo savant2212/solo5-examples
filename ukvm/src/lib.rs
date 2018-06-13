65a49dc2be432ae4f6fcfa4c9228d4dc4b2ce472
Nikita Nazarenko <savant.d@gmail.com>
2018-06-13 23:17:00 +0300

    update memory print

--- ukvm/src/lib.rs
+++ ukvm/src/lib.rs
@@ -6,19 +6,28 @@ extern crate solo5;
 use solo5::alloc::boxed::Box;
 use core::mem;
 
+static TEST : u64 = 0xB16B00B5;
+
 #[no_mangle]
 pub fn rust_main(cmdline : &str) -> isize{
     println!("hello");
     println!("cmdline: {}",  cmdline);
-    
+
+    println!("alloc first");
     let p = Box::new(123 as u32);
+    println!("alloc second");
+    let q = Box::new(456 as u32);
     
+    println!("addresses:");
     println!("boxed value: {}",  p);
+    println!("boxed value: {}",  q);
     unsafe {
         println!("box addr: {:?}",  mem::transmute ::<Box<u32>, *const u8>(p) );
+        println!("box addr: {:?}",  mem::transmute ::<Box<u32>, *const u8>(q) );
         println!("console addr: {:?}",  mem::transmute ::<_, *const u8>(&solo5::CONSOLE) );
+        println!("TEST addr: {:?}",  mem::transmute ::<_, *const u8>(&TEST) );
     }
 
-    return 1;
+    return 0;
 }
 
--- virtio/src/lib.rs
+++ virtio/src/lib.rs
@@ -10,12 +10,18 @@ use core::mem;
 pub fn rust_main(cmdline : &str) -> isize{
     println!("hello");
     println!("cmdline: {}",  cmdline);
-    
+
+    println!("alloc first");
     let p = Box::new(123 as u32);
+    println!("alloc second");
+    let q = Box::new(456 as u32);
     
+    println!("addresses:");
     println!("boxed value: {}",  p);
+    println!("boxed value: {}",  q);
     unsafe {
         println!("box addr: {:?}",  mem::transmute ::<Box<u32>, *const u8>(p) );
+        println!("box addr: {:?}",  mem::transmute ::<Box<u32>, *const u8>(q) );
         println!("console addr: {:?}",  mem::transmute ::<_, *const u8>(&solo5::CONSOLE) );
     }
 
